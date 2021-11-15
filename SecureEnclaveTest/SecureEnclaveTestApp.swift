7f0c3661f6f08812af6fcb31b83a5b3b504ef435
Georg Wechslberger <tauu@h2overclock.de>
2021-11-15 17:33:38 +0100

    Initial Commit

---
+++ SecureEnclaveTest/SecureEnclaveTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureEnclaveTestApp.swift
+//  SecureEnclaveTest
+//
+//  Created by Georg Wechslberger on 15.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SecureEnclaveTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecureEnclaveTestTests/SecureEnclaveTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  SecureEnclaveTestTests.swift
+//  SecureEnclaveTestTests
+//
+//  Created by Georg Wechslberger on 15.11.21.
+//
+
+import XCTest
+@testable import SecureEnclaveTest
+
+class SecureEnclaveTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
